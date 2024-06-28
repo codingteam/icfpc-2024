5234f3cc84d6fa68a89da25b0851952d8dacf183
Friedrich von Never <friedrich@fornever.me>
2024-06-28 16:26:12 +0200

    HttpRequests: process Galaxy language

--- src/HttpRequests.hs
+++ src/HttpRequests.hs
@@ -4,17 +4,30 @@ import Config
 import qualified Data.Text.IO as I
 import           Network.HTTP.Simple
 import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.ByteString.Char8 as BS
 import Data.Text.Encoding (encodeUtf8)
+import Data.Text.Lazy.Encoding (decodeUtf8)
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as L
+import qualified Strings as S
 
 performRequest :: String -> IO ()
 performRequest requestText = do
     token <- readApiToken
     putStrLn $ "Performing request: " ++ requestText
-    let body = BL.pack $ requestText
+    let galaxyBody = T.concat ["S", S.textToGalaxy (T.pack requestText)]
+    let body = encodeUtf8 $ galaxyBody
     req <- parseRequest "POST https://boundvariable.space/communicate"
     let bearerBS = encodeUtf8 $ T.pack $ "Bearer " ++ (T.unpack token)
+
+    BS.putStrLn $ BS.concat ["Sending request in Galaxy: ", encodeUtf8 galaxyBody]
     response <- httpLBS
-     $ setRequestBodyLBS body
+     $ setRequestBodyLBS (BS.fromStrict body)
      $ setRequestHeader "Authorization" [bearerBS] req
-    BL.putStrLn $ getResponseBody response
+    let responseBody = getResponseBody response
+
+    if BL.index responseBody 0 == 'S' then
+        let galaxyEncodedBody = BL.drop 1 responseBody in
+        putStrLn $ "Received response in Galaxy: " ++ (T.unpack $ S.textFromGalaxy $ L.toStrict $ decodeUtf8 $ galaxyEncodedBody)
+    else
+        BL.putStrLn $ BL.concat ["Received raw response: ", responseBody]
