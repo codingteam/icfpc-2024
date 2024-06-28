1c8d686ad1c2a9930c5b429f38b2e15e3255a5dc
Alexander Batischev <eual.jp@gmail.com>
2024-06-28 16:09:26 +0300

    Add parseNumber command to the app

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+import qualified Data.Text as T
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+  args <- getArgs
+  case args of
+    ["parseNumber"] -> do
+      putStrLn "Please enter a number (starting with I):"
+      number <- getLine
+      case parseNumber (T.pack number) of
+        Just x -> print x
+        Nothing -> putStrLn "This is not a number. Perhaps you forgot the \"I\" indicator?"
+
+    _ -> putStrLn "Unknown arguments. See app/Main.hs for a list of available commands"
