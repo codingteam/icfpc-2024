27b9d76d7574a40585e0924c0d5d502cd812b55c
Alexander Batischev <eual.jp@gmail.com>
2024-06-28 17:47:05 +0300

    Implement integer and boolean negation

--- src/AST.hs
+++ src/AST.hs
@@ -33,4 +33,30 @@ data AST =
 
 --- Returns either the reduced AST or the error message explaining what went wrong.
 evalAst :: AST -> Either T.Text AST
-evalAst = undefined
+evalAst input@(Boolean _) = Right input
+evalAst input@(Number _) = Right input
+evalAst input@(Str _) = Right input
+evalAst (Negate (Number input)) = Right $ Number (negate input)
+evalAst (Negate _) = Left "Non-integer input for unary negation"
+evalAst (Not (Boolean input)) = Right $ Boolean (not input)
+evalAst (Not _) = Left "Non-boolean input for \"not\""
+-- evalAst (StrToInt AST)
+-- evalAst (IntToStr AST)
+-- evalAst (Add AST AST)
+-- evalAst (Sub AST AST)
+-- evalAst (Mult AST AST)
+-- evalAst (Div AST AST)
+-- evalAst (Mod AST AST)
+-- evalAst (Lt AST AST)
+-- evalAst (Gt AST AST)
+-- evalAst (Equals AST AST)
+-- evalAst (Or AST AST)
+-- evalAst (And AST AST)
+-- evalAst (Concat AST AST)
+-- evalAst (Take AST AST)
+-- evalAst (Drop AST AST)
+-- evalAst (Apply AST AST)
+-- evalAst (If AST AST AST)
+-- evalAst (Var VarNo)
+-- evalAst (Lambda VarNo AST)
+evalAst _ = undefined
