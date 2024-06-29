43d5224eeb499550a7c35b1681c2c22818ab333d
Alexander Batischev <eual.jp@gmail.com>
2024-06-29 16:30:34 +0300

    Bitcode: use 2 bits per symbol
    (We used to use 2.5 bits/symbol.)
    
    The decompressor grew from 140 to 148 bytes, and we also need to encode
    one more application and a number. However, this all should be swamped
    by the gains we get on inputs longer than about a hundred characters,
    which is the majority of them.
    
    You can re-submit the solutions anytime with:
    
        for i in `seq 21`; do stack run -- http-lambdaman-solution-using-bitcoding $i $(cat data/lambdaman/lambdaman${i}.sol) ; sleep 3 ; done

--- src/StringBitCoding.hs
+++ src/StringBitCoding.hs
@@ -11,19 +11,22 @@ import AST
 --- Encodes a lambdaman solution into an expression which, when evaluated,
 --- produces the initial solution.
 toSelfExtractingBitcode :: T.Text -> AST
-toSelfExtractingBitcode input = Apply bitcodeDecompressor (bitcodeString input)
+toSelfExtractingBitcode input =
+    Apply
+    (Apply bitcodeDecompressor (bitcodeString input))
+    (Number $ fromIntegral $ T.length input)
 
 --- Encodes a lambdaman solution (a string of "UDLR" characters) as a number
 --- (to be decoded on by `bitcodeDecompressor`).
 bitcodeString :: T.Text -> AST
 bitcodeString input = Number $ sum $ zipWith (\m i -> moveToDigit m * base^i) (T.unpack input) ([0..] :: [Integer])
     where
-        base = 5
+        base = 4
 
-        moveToDigit 'U' = 1
-        moveToDigit 'D' = 2
-        moveToDigit 'L' = 3
-        moveToDigit 'R' = 4
+        moveToDigit 'U' = 0
+        moveToDigit 'D' = 1
+        moveToDigit 'L' = 2
+        moveToDigit 'R' = 3
         moveToDigit c = error $ "moveToDigit: unknown move \"" ++ [c] ++ "\""
 
 --- Galaxy program which, given a number, decompressed it into a string of
@@ -35,33 +38,39 @@ bitcodeDecompressor = Apply yCombinator recursiveDecompressor
         input_number = Var 1
         quotinent = Var 2
         decoded_char = Var 3
-        base = Number 5
+        base = Number 4
+        steps_remaining = Var 4
         recursiveDecompressor =
             Lambda 0 -- self-reference
             (Lambda 1 -- input number
-                (Apply
-                    (Lambda 2 -- current quotinent
-                        (Apply
-                            (Lambda 3 -- current decoded character
-                                (If
-                                    (Equals quotinent (Number 0))
-                                    decoded_char
-                                    (Concat
+                (Lambda 4 -- remaining number of steps
+                    (Apply
+                        (Lambda 2 -- current quotinent
+                            (Apply
+                                (Lambda 3 -- current decoded character
+                                    (If
+                                        (Equals steps_remaining (Number 1))
                                         decoded_char
-                                        (Apply self quotinent))
+                                        (Concat
+                                            decoded_char
+                                            (Apply
+                                                (Apply self quotinent)
+                                                (Sub steps_remaining (Number 1))
+                                            )
+                                        )
+                                    )
                                 )
+                                (Apply decodeChar (Mod input_number base))
                             )
-                            (Apply decodeChar (Mod input_number base))
                         )
+                        (Div input_number base)
                     )
-                    (Div input_number base)
                 )
             )
 
         decodeChar =
           Lambda 1 -- the current remainder
-          (Drop (Sub (Var 1) (Number 1))
-            (Take (Var 1) (Str "UDLR")))
+          (Take (Number 1) (Drop (Var 1) (Str "UDLR")))
 
 yCombinator :: AST
 yCombinator = Lambda 0 (Apply (Lambda 1 (Apply (Var 0) (Apply (Var 1) (Var 1)))) (Lambda 2 (Apply (Var 0) (Apply (Var 2) (Var 2)))))
