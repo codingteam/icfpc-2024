906795bab10c3da27757d83232d5356383981efe
Ilya V. Portnov <portnov@bk.ru>
2024-06-29 11:42:54 +0500

    Some optimization.
    Still cannot solve lambdaman4 :/

--- src/Lambdaman.hs
+++ src/Lambdaman.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances, DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances, DeriveGeneric, BangPatterns #-}
 
 module Lambdaman where
 
@@ -12,7 +12,7 @@ import Data.List (findIndex, elemIndex)
 import qualified Data.HashSet as H
 import qualified Data.PQueue.Prio.Min as Q
 
-data Cell = Empty | Pill | Wall | Man
+data Cell = Empty | Pill | Wall
     deriving (Eq, Show, Generic)
 
 instance Hashable Cell
@@ -24,16 +24,19 @@ type Position = (Int, Int)
 type Grid = Array Position Cell
 
 data Problem = Problem {
-    pGrid :: !Grid
+    pGrid :: Grid
   , pPosition :: !Position
   , pOrigin :: !Position
+  , pNPills :: !Value
   }
   deriving (Eq, Show, Generic)
 
 instance Hashable (Array (Int,Int) Cell) where
-    hashWithSalt salt a = hashWithSalt salt $ elems a
+    hashWithSalt salt a = foldr (flip hashWithSalt) salt a
 
-instance Hashable Problem
+instance Hashable Problem where
+    hashWithSalt salt p =
+        hashWithSalt (hashWithSalt salt $ pGrid p) (pPosition p)
 
 type Value = Int
 
@@ -45,13 +48,45 @@ data Path = Path {
 instance Show Path where
     show p = concatMap show $ reverse $ ptSteps p
 
+data ProblemSet = ProblemSet {
+      psPositions :: !(H.HashSet Position)
+    , psNPills :: !(H.HashSet Value)
+    , psGrids :: H.HashSet (Position, Grid)
+  }
+
+-- type ProblemSet = H.HashSet Problem
+
+emptyPS :: ProblemSet
+emptyPS = ProblemSet H.empty H.empty H.empty
+-- emptyPS = H.empty
+
+insertPS :: Problem -> ProblemSet -> ProblemSet
+-- insertPS = H.insert
+
+insertPS p ps =
+    ProblemSet {
+        psPositions = H.insert (pPosition p) (psPositions ps),
+        psNPills = H.insert (pNPills p) (psNPills ps),
+        psGrids = H.insert (pPosition p, pGrid p) (psGrids ps)
+    }
+
+
+memberPS :: Problem -> ProblemSet -> Bool
+-- memberPS = H.member
+
+memberPS p ps =
+--    pNPills p `H.member` psNPills ps &&
+    pPosition p `H.member` psPositions ps &&
+    (pPosition p, pGrid p) `H.member` psGrids ps
+
+
 data AState = AState {
       aOpen :: !(Q.MinPQueue Int Path)
-    , aClosed :: !(H.HashSet Problem)
+    , aClosed :: !ProblemSet
     }
 
 emptyAState :: AState
-emptyAState = AState Q.empty H.empty
+emptyAState = AState Q.empty emptyPS
 
 type A a = StateT AState IO a
 
@@ -61,20 +96,21 @@ calcStep R (y,x) = (y, x+1)
 calcStep D (y,x) = (y+1, x)
 calcStep L (y,x) = (y, x-1)
 
-evalStep :: (Direction, Problem) -> Problem
-evalStep (step, p) =
+evalStep :: Direction -> Problem -> Problem
+evalStep step p =
     let pos' = calcStep step (pPosition p)
     in case pGrid p !? pos' of
         Nothing -> p
         Just Wall -> p
-        _ -> p {pGrid = pGrid p // [(pos', Empty)], pPosition = pos'}
+        Just Pill -> p {pGrid = pGrid p // [(pos', Empty)], pPosition = pos', pNPills = pNPills p - 1}
+        Just Empty -> p {pGrid = pGrid p // [(pos', Empty)], pPosition = pos'}
 
 evalPath :: Problem -> Path -> Problem
 evalPath p path =
     case ptSteps path of
         [] -> p
         (step : steps) ->
-            let p' = evalStep (step, p)
+            let p' = evalStep step p
                 path' = path {ptSteps = steps}
             in  evalPath p' (appendPath step p' path')
 
@@ -87,15 +123,18 @@ successors p = mapMaybe check [U, R, D, L]
         check step =
             let pos' = calcStep step (pPosition p)
             in  case pGrid p !? pos' of
-                    Just Pill -> Just (step, evalStep (step, p))
-                    Just Empty -> Just (step, evalStep (step, p))
+                    Just Pill -> Just (step, evalStep step p)
+                    Just Empty -> Just (step, evalStep step p)
                     _ -> Nothing
+
+calcNPills :: Grid -> Value
+calcNPills grid = length $ filter (== Pill) $ A.elems grid
 
 calcPriority' :: Path -> (Value, Value)
 calcPriority' path = (originToCurrent, currentToGoal)
     where
         originToCurrent = length $ ptSteps path
-        currentToGoal = length $ filter (== Pill) $ A.elems (pGrid $ ptState path)
+        currentToGoal = pNPills $ ptState path
 
 appendPath :: Direction -> Problem -> Path -> Path
 appendPath step p path =
@@ -110,7 +149,7 @@ extractPath path = ptState path
 aStar :: Problem -> A (Maybe Path)
 aStar p = do
         let queue = Q.singleton 0 $ singletonPath p
-        put $ AState queue H.empty
+        put $ AState queue emptyPS
         loop
     where
         loop = do
@@ -122,14 +161,14 @@ aStar p = do
                     modify $ \st -> st {aOpen = rest}
                     let grid = extractPath path
                     closed <- gets aClosed
-                    if grid `H.member` closed
+                    if grid `memberPS` closed
                       then do
                         -- liftIO $ putStrLn "we were here already, skip"
                         loop
                       else if isGoal $ pGrid grid
                              then return $ Just path
                              else do
-                               modify $ \st -> st {aClosed = H.insert grid (aClosed st)}
+                               modify $ \st -> st {aClosed = insertPS grid (aClosed st)}
                                forM_ (successors grid) $ \(step, y) -> do
                                  let path' = appendPath step y path
                                  let (priority1, priority2) = calcPriority' path'
@@ -165,7 +204,8 @@ decodeProblem lines =
         grid :: Grid
         grid = A.array ((0,0), (sizeY-1, sizeX-1)) $ zip indices lines'
         origin = (originY, originX)
-    in  Problem grid origin origin
+        nPills = calcNPills grid
+    in  Problem grid origin origin nPills
 
 showProblem :: Problem -> String
 showProblem p =
