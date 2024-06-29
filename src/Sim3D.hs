ec050279e68984f379bb67134c29e34826227380
Alexander Batischev <eual.jp@gmail.com>
2024-06-29 20:08:44 +0300

    Fix whitespace

--- src/Sim3D.hs
+++ src/Sim3D.hs
@@ -4,8 +4,8 @@ import qualified Data.Char as C
 import qualified Data.Vector as V
 import qualified Data.Text as DT
 import qualified Data.Text.IO as DTI
-  
-data Cell = 
+
+data Cell =
       Empty
     | Value Int
     | MoveLeft
@@ -24,7 +24,7 @@ data Cell =
     | InputA
     | InputB
     deriving (Eq, Show)
-  
+
 type Board = V.Vector (V.Vector Cell)
 
 isNumber :: DT.Text -> Bool
@@ -50,7 +50,7 @@ readCell "@" = TimeWarp
 readCell "=" = Equal
 readCell "#" = NotEqual
 readCell "S" = OutputS
-readCell "A" = InputA 
+readCell "A" = InputA
 readCell "B" = InputB
 
 parseBoard :: DT.Text -> Board
@@ -62,10 +62,10 @@ parseBoard contents =
 readBoard :: String -> IO Board
 readBoard path = do
     contents <- DTI.readFile path
-    return $ parseBoard contents    
+    return $ parseBoard contents
 
 type Inputs = (Int, Int)
- 
+
 replaceInputs :: Board -> Inputs -> Board
 replaceInputs board (a, b) =
     let replaceCell InputA = Value a
@@ -97,10 +97,10 @@ printBoard board = do
             V.mapM_ (\x -> do
                 printCell x
                 putStr " ") row
-            
+
             putStrLn ""
     V.mapM_ printRow board
-  
+
 simulate :: String -> Inputs -> IO ()
 simulate boardPath inputs = do
     board <- readBoard boardPath
@@ -110,8 +110,8 @@ simulate boardPath inputs = do
         putStrLn "I cannot see the goal on the initial board. This game will never end."
         doSimulation board' False
     else
-        doSimulation board' True 
-        
+        doSimulation board' True
+
 doSimulation :: Board -> Bool -> IO ()
 doSimulation board checkGameOver = do
     printBoard board
@@ -120,7 +120,7 @@ doSimulation board checkGameOver = do
     else do
         putStrLn "Press enter to continue."
         _ <- getLine
-        doSimulation (simulateStep board) checkGameOver    
+        doSimulation (simulateStep board) checkGameOver
 
 isGameOver :: Board -> Bool
 isGameOver board = True
@@ -137,11 +137,11 @@ produceCellUpdate (x, y) board =
     let cell = (board V.! y) V.! x in
     case cell of
         MoveLeft -> moveCell (x + 1, y) (x - 1, y) board
-        MoveRight -> moveCell (x - 1, y) (x + 1, y) board 
-        MoveUp -> moveCell (x, y - 1) (x, y + 1) board 
+        MoveRight -> moveCell (x - 1, y) (x + 1, y) board
+        MoveUp -> moveCell (x, y - 1) (x, y + 1) board
         MoveDown -> moveCell (x, y + 1) (x, y - 1) board
         Value _ -> []
-        Empty -> [] 
+        Empty -> []
         _ -> error $ "Unexpected cell: " ++ show cell ++ " at " ++ show (x, y) ++ " in " ++ show board ++ "."
 
     where moveCell (x1, y1) (x2, y2) board = [(x1, y1, Empty), (x2, y2, (board V.! y1) V.! x1)]
@@ -159,4 +159,3 @@ applyUpdates board ((x, y, cell):updates) =
         newRow = row V.// [(x, cell)]
         newBoard = board V.// [(y, newRow)] in
     applyUpdates newBoard updates
-    
