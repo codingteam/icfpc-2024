351ccc527da68a8ae38d052ba1f915dc19ae1ce6
Igor S. Gerasimov <i.s.ger@ya.ru>
2024-06-30 11:49:58 +0200

    Update verify_lambdaman.py

--- data/lambdaman/verify_lambdaman.py
+++ data/lambdaman/verify_lambdaman.py
@@ -25,8 +25,10 @@ def to_2D(task):
 
 
 def print_field(task2D):
+    print("#" * (len(task2D) + 2))
     for line in task2D:
-        print(line)
+        print("#" + "".join(line) + "#")
+    print("#" * (len(task2D) + 2))
 
 
 def do_instructions(task2D, solution):
@@ -40,37 +42,50 @@ def do_instructions(task2D, solution):
     for i in range(0, len(solution)):
         c = solution[i]
         if c == 'U':
+            if x == 0:
+                continue
             if task2D[x-1][y] == '#':
-                print(f'command {i}:{c} is improper!')
+#                print(f'command {i}:{c} is improper!')
+                pass
             else:
                 task2D[x][y] = ' '
                 x -= 1
                 task2D[x][y] = 'L'
         elif c == 'D':
-            if task2D[x+1][y] == '#':
-                print(f'command {i}:{c} is improper!')
+            if x + 1 == len(task2D):
+                continue
+            elif task2D[x+1][y] == '#':
+#                print(f'command {i}:{c} is improper!')
+                pass
             else:
                 task2D[x][y] = ' '
                 x += 1
                 task2D[x][y] = 'L'
         elif c == 'L':
-            if task2D[x][y-1] == '#':
-                print(f'command {i}:{c} is improper!')
+            if y == 0:
+                continue
+            elif task2D[x][y-1] == '#':
+#                print(f'command {i}:{c} is improper!')
+                pass
             else:
                 task2D[x][y] = ' '
                 y -= 1
                 task2D[x][y] = 'L'
         elif c == 'R':
-            if task2D[x][y+1] == '#':
-                print(f'command {i}:{c} is improper!')
+            if y + 1 == len(task2D[x]):
+              continue
+            elif task2D[x][y+1] == '#':
+#                print(f'command {i}:{c} is improper!')
+                pass
             else:
                 task2D[x][y] = ' '
                 y += 1
                 task2D[x][y] = 'L'
         else:
             print(f'command {i}:{c} is improper!')
-    print(f"Solution cost: {len(solution)+18}")
-    print(solution)
+            pass
+#    print(solution)
+    print(f"Approx. solution cost: {len(solution)+18}")
     return task2D
 
 
