8104aa329e6e01ad696cd14e8c48ec32fb588443
Alexander Batischev <eual.jp@gmail.com>
2024-07-01 13:51:10 +0300

    Add lambdaman6 solution of 87 bytes

--- src/Lambdaman6.hs
+++ src/Lambdaman6.hs
@@ -1,33 +1,14 @@
-module Lambdaman6 where
+module Lambdaman6 (lambdaman6Solution) where
 
 import AST
-import StringBitCoding (makeRecursion)
 
 double :: AST
 double = 0 --> Concat input input
     where
     input = Var 0
 
-times :: AST
-times = makeRecursion go
-    where
-    self = Var 0
-    action = Var 1
-    input = Var 2
-    iterations_left = Var 3
-
-    go =
-        0 --> -- self
-        1 --> -- action
-        2 --> -- input
-        3 --> -- iterations_left
-        (If
-            (Equals iterations_left 1)
-            (action $$ input)
-            (action $$ (((self $$ action) $$ input) $$ (iterations_left - 1))))
-
 lambdaman6Solution :: AST
 lambdaman6Solution =
     Concat
         "solve lambdaman6 "
-        (((times $$ double) $$ "R") $$ 8)
+        (((0 --> 1 --> (Var 0 $$ (Var 0 $$ ((Var 0) $$ ((Var 0) $$ (Var 1)))))) $$ double) $$ "RRRRRRRRRRRRR")
